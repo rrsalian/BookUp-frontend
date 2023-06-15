680d7a9505cd313970d90fe25121bcf13707ec4f
Sara Evans <ssevans@umich.edu>
2023-06-15 16:41:19 -0400

    map toggle

--- bookup-frontend/src/components/BookList/BookList.tsx
+++ bookup-frontend/src/components/BookList/BookList.tsx
@@ -1,13 +1,17 @@
 import { BookCard } from "../BookCard/BookCard";
 import "./BookList.css"
 import { BookMap } from "../BookMap/BookMap";
+import { useState } from "react";
 
 
 export function BookList(props: {bookList: any[]}) {
 
+    const [showMap, setShowMap] = useState(false);
+    let mapCon = showMap ?  <BookMap></BookMap> : "";
     return (
        <div> 
-        <BookMap></BookMap>
+        <button onClick = {()=> setShowMap(!showMap)}> Toggle Map</button>
+        {mapCon}
         <div className="booklist">
             {
                 props.bookList.map((book, index) => <BookCard key={index} book={book}/>)
