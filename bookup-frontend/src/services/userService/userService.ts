5f11a8520092605e1669ed5b0f50461ebf9ad783
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-06-28 20:11:51 -0400

    Push UserService for getUsersByIsbn

--- bookup-frontend/src/services/userService/userService.ts
+++ bookup-frontend/src/services/userService/userService.ts
@@ -17,6 +17,11 @@ export async function getUserByEmail(email: string): Promise<Buser> {
     return response.data;
 }
 
+export async function getUserByIsbn(isbn: string): Promise<Buser> {
+    const response = await axios.get<Buser>(`https://us-central1-bookup-4c4c7.cloudfunctions.net/api/users/book/${isbn}`);
+    return response.data;
+}
+
 export const addUser = async (user: Buser): Promise<Buser> => {
     return (await axios.post('https://us-central1-bookup-4c4c7.cloudfunctions.net/api/users', user)).data;
 };
