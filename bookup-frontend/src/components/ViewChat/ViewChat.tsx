d6953737d57ac37cb31229aae25fd5e6b2a79472
Grif Wakeman <grif@syncbeast.co>
2023-07-05 12:08:24 -0400

    added some css to chat, not finished yet.

--- bookup-frontend/src/components/ViewChat/ViewChat.tsx
+++ bookup-frontend/src/components/ViewChat/ViewChat.tsx
@@ -14,90 +14,94 @@ export function ViewChat(props: { currentUser: Buser, chatUser: Buser, isbn: str
 
     useEffect(() => {
         if (me) {
-            getUserMessages(isbn, me._id!, other._id!);            
+            getUserMessages(isbn, me._id!, other._id!);
         }
-            
+
     }, [messages.length])
 
     function scrollToBottom() {
         const chat = document.getElementById("chatList")!;
         chat.scrollTop = chat?.scrollHeight;
-      };
+    };
 
     async function getUserMessages(isbn: string, user1_id: string, user2_id: string) {
-        const userMessages = await getMessagesByUserBookID(isbn, user1_id, user2_id);        
-        setMessages(userMessages);        
+        const userMessages = await getMessagesByUserBookID(isbn, user1_id, user2_id);
+        setMessages(userMessages);
         props.handleMessages(userMessages);
-        
+
     }
 
-    async function handleSubmit (e: any) {
+    async function handleSubmit(e: any) {
         e.preventdefault;
         const date = new Date(); // Or the date you'd like converted.
         const isoDateTime = new Date(date.getTime() - (date.getTimezoneOffset() * 60000));
-        
-        let initiator:string = (messages[0]) ? messages[0].initiator!: props.currentUser._id!;
-        let currentState = (messages.length === 0) ? "invite" : ""
-                
-        const newMessage:Message = { "createdAt": isoDateTime, 
-                                     "initiator": initiator,
-                                     "isbn": isbn,
-                                     "receiverId": other._id!,
-                                     "senderId": me._id!,
-                                     "state": currentState,
-                                     "swapToIsbn": "",                       
-                                     "message": newText
-                                    };
-        
-        await postMessage(newMessage);
-        const newMessages: Message[] = [ ...messages, newMessage];
-        setMessages( newMessages );
-        setNewText("");        
-        scrollToBottom();        
-        props.handleMessages(messages)
-               
-      };
 
-      const handleChange = (e: any) => {
-        setNewText( e.target.value );
-      };
+        let initiator: string = (messages[0]) ? messages[0].initiator! : props.currentUser._id!;
+        let currentState = (messages.length === 0) ? "invite" : ""
+
+        const newMessage: Message = {
+            "createdAt": isoDateTime,
+            "initiator": initiator,
+            "isbn": isbn,
+            "receiverId": other._id!,
+            "senderId": me._id!,
+            "state": currentState,
+            "swapToIsbn": "",
+            "message": newText
+        };
+
+        await postMessage(newMessage);
+        const newMessages: Message[] = [...messages, newMessage];
+        setMessages(newMessages);
+        setNewText("");
+        scrollToBottom();
+        props.handleMessages(messages)
+
+    };
+
+    const handleChange = (e: any) => {
+        setNewText(e.target.value);
+    };
 
 
     return (
         <div className="main">
-            <div className="chatSpace">                
+            <div className="chatSpace">
+                <h3>Chat Messages</h3>
                 <div className="chatWindow">
+
                     <ul className="chat" id="chatList">
-                        <h3>Chat Messages</h3>
-                        {                            
+
+                        {
                             messages.map(message =>
-                            <div key={message._id}>
-                                {me._id === message.senderId ?
-                                (
-                                    <li className="self">
-                                        <div className="msg">
-                                            <p>Me:</p>
-                                            <div>{message.message}</div>                                            
-                                        </div>
-                                    </li>
-                                ) :
-                                (
-                                    <li className="other">
-                                        <div className="msg">
-                                           <p>{other.email}</p>
-                                            <div className="message">{message.message}</div>
-                                        </div>
-                                    </li>
-                                )}
-                            </div>
+                                <div key={message._id}>
+                                    {me._id === message.senderId ?
+                                        (
+                                            <div className="self">
+                                                <div className="msg">
+                                                    <p>Me:</p>
+                                                    <div>{message.message}</div>
+                                                </div>
+                                            </div>
+                                        ) :
+                                        (
+                                            <div className="other">
+                                                <div className="msg">
+                                                    <p>{other.email}</p>
+                                                    <div className="message">{message.message}</div>
+                                                </div>
+                                            </div>
+                                        )}
+                                </div>
                             )
                         }
                     </ul>
-                    <div className="chatInputWrapper">                    
-                        <input className="textarea input" type="text" value={newText} placeholder="Enter your message..."
-                        onChange={handleChange}/>
-                        <button onClick={handleSubmit}>Send</button>
-                    </div>
+
+                </div>
+                <div className="chatInputWrapper">
+                    <input className="textarea input" type="text" value={newText} placeholder="Enter your message..."
+                        onChange={handleChange} />
+                    <button onClick={handleSubmit}>Send</button>
                 </div>
             </div>
         </div>
